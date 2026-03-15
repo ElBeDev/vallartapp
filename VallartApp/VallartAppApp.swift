26408f6b2cbfc492f01d38a925962242d6fbf0b8
Eduardo Benavides <elbedev90@gmail.com>
2026-03-14 18:28:23 -0600

    Initial Commit

---
+++ VallartApp/VallartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VallartAppApp.swift
+//  VallartApp
+//
+//  Created by Bernardo Ramírez on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct VallartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
